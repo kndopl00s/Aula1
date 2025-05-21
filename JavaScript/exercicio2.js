0a3ed391212dc255630a2ff211a324c346ee47aa
gabriel alejandro e kayna rodrigues <0000941456@senaimgaluno.com.br>
2025-05-21 17:04:23 -0300

    modificação dos arquivos 2

--- JavaScript/exercicio2.js
+++ JavaScript/exercicio2.js
@@ -2,7 +2,7 @@ let prompt = require('prompt-sync') ();
 let nome = prompt("Qual é o seu nome?");
 let idade = parseInt(prompt("Qual é a sua idade?"));
 
-if (idade >= 18) {
+if (idade >= 17) {
     console.log("Olá, " + nome + "! Você é maior de idade.");
 } else {
     console.log("Olá, " + nome + "! Você é menor de idade.");
